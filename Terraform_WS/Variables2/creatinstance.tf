903b4495b543ea06609d9a3bdb834efa0cf94d53
Ramgadiyasplk <tirumalagadiya@gmail.com>
2024-08-25 12:41:38 +0530

    addindg AMIS and region

--- Terraform_WS/Variables2/creatinstance.tf
+++ Terraform_WS/Variables2/creatinstance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = "ami-0b0ea68c435eb488d"
+  ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
 
   tags = {
